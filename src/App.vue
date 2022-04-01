e548f5ba4bd148c89940278a1455cc34645d7b77
kanchan7th <kanchanbhuju@gmail.com>
2022-04-01 22:23:51 +0545

    import card

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProfileCard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileCard from './components/ProfileCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProfileCard
   }
 }
 </script>
